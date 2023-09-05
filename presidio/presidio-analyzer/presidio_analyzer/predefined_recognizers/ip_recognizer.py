c12375fdf8ae0cd3a6443a209ef082d83a7e1434
kalpa093 <127818884+kalpa093@users.noreply.github.com>
2023-09-05 15:34:17 +0900

    Update into RRN regex

--- presidio/presidio-analyzer/presidio_analyzer/predefined_recognizers/ip_recognizer.py
+++ presidio/presidio-analyzer/presidio_analyzer/predefined_recognizers/ip_recognizer.py
@@ -1,4 +1,3 @@
-import ipaddress
 from typing import Optional, List
 
 from presidio_analyzer import Pattern, PatternRecognizer
@@ -6,7 +5,7 @@ from presidio_analyzer import Pattern, PatternRecognizer
 
 class IpRecognizer(PatternRecognizer):
     """
-    Recognize IP address using regex.
+    Recognize Resident Registration Number using regex.
 
     :param patterns: List of patterns to be used by this recognizer
     :param context: List of context words to increase confidence in detection
@@ -16,29 +15,19 @@ class IpRecognizer(PatternRecognizer):
 
     PATTERNS = [
         Pattern(
-            "IPv4",
-            r"\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b",  # noqa: E501
-            0.6,
-        ),
-        Pattern(
-            "IPv6",
-            r"\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\b",  # noqa: E501
-            0.6,
-        ),
-        Pattern(
-            "IPv6",
-            r"::",
+            "Resident Registration Number",
+            r"\d{2}([0][1-9]|[1][0-2])([0][1-9]|[1-2]\d|[3][0-1])-?([1-4]{1})([0-9]{6})",
             0.1,
         ),
     ]
 
-    CONTEXT = ["ip", "ipv4", "ipv6"]
+    CONTEXT = ["RNN, "Resident Registration Number", "주민등록번호"]
 
     def __init__(
         self,
         patterns: Optional[List[Pattern]] = None,
         context: Optional[List[str]] = None,
-        supported_language: str = "en",
+        supported_language: str = "ko",
         supported_entity: str = "IP_ADDRESS",
     ):
         patterns = patterns if patterns else self.PATTERNS
@@ -50,14 +39,3 @@ class IpRecognizer(PatternRecognizer):
             supported_language=supported_language,
         )
 
-    def invalidate_result(self, pattern_text: str) -> bool:
-        """
-        Check if the pattern text cannot be validated as an IP address.
-
-        :param pattern_text: Text detected as pattern by regex
-        :return: True if invalidated
-        """
-        try:
-            ipaddress.ip_address(pattern_text)
-        except ValueError:
-            return True
